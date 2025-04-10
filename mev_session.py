8d56329c1159efb63fc9952d0de53f37654664f0
Manuel Escriche <manuel.escriche.vicente@gmail.com>
2025-04-10 08:42:02 +0200

    Updated documentation
    Updated documentation

--- mev_session.py
+++ mev_session.py
@@ -1,12 +1,12 @@
 import os
 from datamodel import UserData, AccountsTree
 from dbase import db_open, Session, Account, Transaction, BookEntry
-
+from sqlalchemy import select
 root = os.getcwd()
 user = UserData(root, 'mev')
 db_open(user.db_config)
 db = Session()
 accounts = db.query(Account).all()
-trans = db.query(Transaction).all()
+transactions = db.query(Transaction).all()
 entries = db.query(BookEntry).all()
 tree = AccountsTree.from_db()
